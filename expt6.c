ae231778759ea6f38a706d3c2868d4c5415a4126
saurabhj-arch <saurabh.jagdale2006@gmail.com>
2025-04-29 00:56:57 +0530

    Add files via upload

--- expt6.c
+++ expt6.c
@@ -21,8 +21,7 @@ int main()
 		printf("\n1.Create a list");
 		printf("\n2.Display list");
 		printf("\n3.Add a node at beginning");
-		printf("\n4.Add a node at end");
-		printf("\n5.Exit");
+		printf("\n4.Exit");
 		printf("\n\nEnter your option: ");
 		scanf("%d", &option);
 		
@@ -35,9 +34,8 @@ int main()
 				break;
 			case 3: start = insert_beg(start);
 				break;
-			case 4: start = insert_end(start);
 		}
-	}while(option != 5);
+	}while(option != 4);
 	return 0;
 }
 
@@ -93,20 +91,6 @@ struct node *insert_beg(struct node *start)
 	start = new_node;
 	return start;
 }
-struct node *insert_end(struct node *start)
-{
-	struct node *ptr,*new_node;
-	int num;
-	printf("\n Enter the data: ");
-	scanf("%d",&num);
-	new_node = (struct node *)malloc(sizeof(struct node));
-	new_node -> data = num;
-	new_node -> next = NULL;
-	ptr = start;
-	while(ptr -> next != NULL)
-		ptr = ptr->next;
-		ptr->next = new_node;
-		return start;
-}
+
 	
 
