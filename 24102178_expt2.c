8b12368b79af2e418f1e1d29f81b2133012426a1
saurabhj-arch <saurabh.jagdale2006@gmail.com>
2025-04-28 23:47:30 +0530

    expt2

---
+++ 24102178_expt2.c
@@ -0,0 +1,16 @@
+// C program to find factorial of given number
+// using recursion
+#include <stdio.h>
+unsigned int factorial(unsigned int n) {
+// Base Case:
+if (n == 1) {
+return 1;
+}
+// Multiplying the current N with the previous product of Ns (Recursive case)
+return n * factorial(n - 1);
+}
+int main() {
+int num = 5;
+printf("Factorial of %d is %d", num, factorial(num));
+return 0;
+}
